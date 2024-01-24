6593f7004c9f7b0bd6453529eb4822ed394b0942
illyForeverEarly <ilyas.asylbekov1305@gmail.com>
2024-01-25 00:02:08 +0600

    Add 3 questions

--- src/questionPool.js
+++ src/questionPool.js
@@ -2,7 +2,22 @@ const questionPool = [
     {
       id: 1,
       text: 'What is the last known bounty of William?',
-      correctAnswer: 60000000
+      correctAnswer: 60000000,
+    },
+    {
+      id: 2,
+      text: 'What is the last known bounty of Ragnar?',
+      correctAnswer: 350000000,
+    },
+    {
+      id: 3,
+      text: 'What is the last known bounty of Duros?',
+      correctAnswer: 30000000,
+    },
+    {
+      id: 4,
+      text: 'What is the last known bounty of Verona?',
+      correctAnswer: 299000000,
     },
   ];
   
